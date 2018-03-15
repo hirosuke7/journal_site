e92a4739e8ba3915275c8e474cd6e1a58648849a
hirosuke7 <captif7_de_2_anjelot8@ezweb.ne.jp>
2018-03-15 12:59:42 +0900

    プルリク用

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,12 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-
   def index
     @articles = Article.all
   end
-
   def new
     @article = Article.new
   end
-
   def create
     @article = Article.new(article_params)
     @article.save
@@ -20,13 +17,11 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
-
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
     redirect_to articles_path
   end
-
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
